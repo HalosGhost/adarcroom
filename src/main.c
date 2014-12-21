dd576cae07e7bcc934b8da6573b54b77691d764e
halosghost <halosghost@archlinux.info>
2014-12-21 17:50:03 -0600

    Add variable for current location; detect screen size and do some (naïve) centering

--- src/main.c
+++ src/main.c
@@ -19,18 +19,26 @@ main (void) { // Eventually offer argument parsing?
 
     enum WIN_TYPE {
         LOG, TRVL, INV, ROOM, OTSD, PATH, WRLD, SHIP
-    };
+    } cur_loc = ROOM;
 
     WINDOW * wins [SHIP + 1];
 
     initscr(); noecho(); cbreak(); curs_set(0);
     refresh();
 
-    wins[LOG] = newwin(LINES - 1, 35, 1, 1);
+    signed cc = COLS / 2, cr = LINES / 2;
+
+    if ( COLS < 149 ) {
+        printw("The classic interface requires a console at least 149 columns wide");
+        getch(); endwin(); exit(1);
+    }
+
+    wins[LOG] = newwin(LINES - 1, 35, 1, cc - 71);
     scrollok(wins[LOG], TRUE);
-    wins[TRVL] = newwin(1, 69, 1, 38);
-    wins[ROOM] = newwin(LINES - 2, 69, 2, 38);
-    wins[INV] = newwin(LINES - 1, 38, 1, 109);
+    wins[TRVL] = newwin(1, 69, 1, cc - 34);
+    wins[ROOM] = newwin(LINES - 2, 69, 2, cc - 34);
+    mvwprintw(wins[ROOM], 1, 1, "cc = %d, cr = %d", cc, cr);
+    wins[INV] = newwin(LINES - 1, 38, 1, cc + 37);
     box(wins[ROOM], 0, 0);
     box(wins[INV], 0, 0);
 
@@ -43,9 +51,9 @@ main (void) { // Eventually offer argument parsing?
     wprintw(wins[TRVL], " | An Old Starship");
 
     // Refresh Windows
-    for ( enum WIN_TYPE i = 0; i <= ROOM; i ++ ) {
+    for ( enum WIN_TYPE i = 0; i <= INV; i ++ ) {
         wrefresh(wins[i]);
-    }
+    } wrefresh(wins[cur_loc]); // only refresh currently selected main window
 
     for ( ; getch() != 'q'; );
     for ( enum WIN_TYPE i = 0; i <= SHIP; i ++ ) {
